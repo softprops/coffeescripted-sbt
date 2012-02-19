0abbe91e38bc3b6ae8207bc15da47756554af6a6
softprops <d.tangren@gmail.com>
2012-02-19 00:37:50 -0500

    well, let's not go that far. we actually need to provide a means of explicitly telling the compiler which iced runtime mode we're using to avoid runtime dependencies

--- src/main/scala/compiler.scala
+++ src/main/scala/compiler.scala
@@ -12,6 +12,8 @@ import java.nio.charset.Charset
 abstract class Compiler(src: String) {
   val utf8 = Charset.forName("utf-8")
 
+  def extendedArgs: Map[String, String] = Map.empty[String, String]
+
   /**
    * Compiles a string of Coffeescript code to Javascript.
    *
@@ -22,7 +24,10 @@ abstract class Compiler(src: String) {
   def compile(code: String, bare: Boolean): Either[String, String] = withContext { ctx =>
     val coffee = scope.get("CoffeeScript", scope).asInstanceOf[NativeObject]
     val compileFunc = coffee.get("compile", scope).asInstanceOf[Function]
-    val opts = ctx.evaluateString(scope, "({bare: %b});".format(bare), null, 1, null)
+    val opts = ctx.evaluateString(scope, "({bare: %b%s});".format(
+      bare, if(extendedArgs.isEmpty) "" else "," + (extendedArgs.map {
+        case (k, v) => "%s:'%s'".format(k, v)
+    } mkString(","))), null, 1, null)
     try {
       Right(compileFunc.call(ctx, scope, coffee, Array(code, opts)).asInstanceOf[String])
     } catch {
@@ -58,4 +63,6 @@ abstract class Compiler(src: String) {
 
 object Vanilla extends Compiler("vanilla/coffee-script.js")
 
-object Iced extends Compiler("iced/coffee-script.js")
+object Iced extends Compiler("iced/coffee-script.js") {
+  override def extendedArgs = Map("runtime" -> "inline")
+}
