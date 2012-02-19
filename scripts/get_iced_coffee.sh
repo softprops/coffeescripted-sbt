2502ae3665da289d4f20bf5516e3e06f1c6e70e5
softprops <d.tangren@gmail.com>
2012-02-18 23:36:06 -0500

    iced p marks the spot looks like maxtaco fixed the vendoring issue

--- scripts/get_iced_coffee.sh
+++ scripts/get_iced_coffee.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo "fetching latest iced coffeescript compiler"
+CS=http://maxtaco.github.com/coffee-script/extras/coffee-script.js
 
-if [[ -f src/main/resources/iced/coffee-script-iced.js && ! -f src/main/resources/iced/coffee-script.js.released ]]; then
+if [[ -f src/main/resources/iced/coffee-script.js && ! -f src/main/resources/iced/coffee-script.js.released ]]; then
   echo "stashing old iced coffee"
-  mv src/main/resources/iced/coffee-script-iced.js src/main/resources/iced/coffee-script-iced.js.released
+  mv src/main/resources/iced/coffee-script.js src/main/resources/iced/coffee-script.js.released
 fi
 
-curl -o src/main/resources/iced/coffee-script.js --progress-bar -O http://maxtaco.github.com/coffee-script/extras/coffee-script.js
+curl -o src/main/resources/iced/coffee-script.js --progress-bar -O $CS
