bc0f52f40970da417503cd27a46d2a2b7df627f0
softprops <d.tangren@gmail.com>
2012-02-26 22:49:10 -0500

    update iced test fixtures with new 'inline' iced runtime fix

--- src/sbt-test/coffeescripted-sbt/iced/fixtures/bar.js
+++ src/sbt-test/coffeescripted-sbt/iced/fixtures/bar.js
@@ -1,7 +1,42 @@
 (function() {
-  var iced, parallelSearch, __iced_k;
+  var iced, parallelSearch, __iced_k,
+    __slice = [].slice;
 
-  iced = require('iced-coffee-script').iced;
+  iced = {
+    Deferrals: (function() {
+
+      function _Class(_arg) {
+        this.continuation = _arg;
+        this.count = 1;
+        this.ret = null;
+      }
+
+      _Class.prototype._fulfill = function() {
+        if (!--this.count) return this.continuation(this.ret);
+      };
+
+      _Class.prototype.defer = function(defer_params) {
+        var _this = this;
+        ++this.count;
+        return function() {
+          var inner_params, _ref;
+          inner_params = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+          if (defer_params != null) {
+            if ((_ref = defer_params.assign_fn) != null) {
+              _ref.apply(null, inner_params);
+            }
+          }
+          return _this._fulfill();
+        };
+      };
+
+      return _Class;
+
+    })(),
+    findDeferral: function() {
+      return null;
+    }
+  };
   __iced_k = function() {};
 
   parallelSearch = function(keywords, cb) {
