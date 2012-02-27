ad3ccc33cb4b7b3f21d4f10999e092ca8e85b6d8
softprops <d.tangren@gmail.com>
2012-02-26 22:47:03 -0500

    refactor how custom compilers declare compile arguments

--- src/main/scala/compiler.scala
+++ src/main/scala/compiler.scala
@@ -1,6 +1,7 @@
 package coffeescript
 
-import org.mozilla.javascript.{Context, Function, JavaScriptException, NativeObject}
+import org.mozilla.javascript.{
+  Context, Function, JavaScriptException, NativeObject }
 import java.io.InputStreamReader
 import java.nio.charset.Charset
 
@@ -12,57 +13,70 @@ import java.nio.charset.Charset
 abstract class Compiler(src: String) {
   val utf8 = Charset.forName("utf-8")
 
-  def extendedArgs: Map[String, String] = Map.empty[String, String]
+  /** compiler arguments in addition to `bare` */
+  def args: Map[String, Any] = Map.empty[String, Any]
+
+  override def toString = "%s(%s)" format(getClass.getSimpleName, src)
 
   /**
    * Compiles a string of Coffeescript code to Javascript.
    *
    * @param code the Coffeescript source code
    * @param bare whether the Coffeescript compiler should run in "bare" mode
-   * @return Either a compilation error description or the compiled Javascript code
+   * @return Either a compilation error description or
+   *   the compiled Javascript code
    */
-  def compile(code: String, bare: Boolean): Either[String, String] = withContext { ctx =>
-    val coffee = scope.get("CoffeeScript", scope).asInstanceOf[NativeObject]
-    val compileFunc = coffee.get("compile", scope).asInstanceOf[Function]
-    val opts = ctx.evaluateString(scope, "({bare: %b%s});".format(
-      bare, if(extendedArgs.isEmpty) "" else "," + (extendedArgs.map {
-        case (k, v) => "%s:'%s'".format(k, v)
-    } mkString(","))), null, 1, null)
-    try {
-      Right(compileFunc.call(ctx, scope, coffee, Array(code, opts)).asInstanceOf[String])
-    } catch {
-      case e : JavaScriptException =>
-        Left(e.getValue.toString)
+  def compile(code: String, bare: Boolean): Either[String, String] =
+    withContext { ctx =>
+      val coffee = scope.get("CoffeeScript", scope).asInstanceOf[NativeObject]
+      val compileFunc = coffee.get("compile", scope).asInstanceOf[Function]
+      val opts = ctx.evaluateString(scope, jsArgs(bare), null, 1, null)
+      try {
+        Right(compileFunc.call(
+          ctx, scope, coffee, Array(code, opts)).asInstanceOf[String])
+      } catch {
+        case e : JavaScriptException =>
+          Left(e.getValue.toString)
+      }
     }
-  }
 
   lazy val scope = withContext { ctx =>
     val scope = ctx.initStandardObjects()
     ctx.evaluateReader(
       scope,
-      new InputStreamReader(getClass().getResourceAsStream("/%s" format src), utf8),
-      src, 1, null
+      new InputStreamReader(
+        getClass().getResourceAsStream("/%s" format src), utf8
+      ), src, 1, null
     )
 
     scope
   }
 
-  private def withContext[T](f: Context => T): T = {
-    val ctx = Context.enter()
+  private def withContext[T](f: Context => T): T =
     try {
-      ctx.setOptimizationLevel(-1) // Do not compile to byte code (max 64kb methods)
+      val ctx = Context.enter()
+      // Do not compile to byte code (max 64kb methods)
+      ctx.setOptimizationLevel(-1)
       f(ctx)
-    } catch {
-      case e =>
-        throw e
     } finally {
       Context.exit()
     }
-  }
+
+  private def jsArgs(bare: Boolean) =
+    ((List.empty[String] /: (Map("bare" -> bare) ++ args)) {
+      (a,e) => e match {
+        case (k, v) =>
+          "%s:%s".format(k, v match {
+            case s: String => "'%s'" format s
+            case lit => lit
+          }) :: a
+        }
+    }).mkString("({",",","});")
+
 }
 
 object Vanilla extends Compiler("vanilla/coffee-script.js")
 
 object Iced extends Compiler("iced/coffee-script.js") {
-  override def extendedArgs = Map("runtime" -> "inline")
+  override def args = Map("runtime" -> "inline")
 }
